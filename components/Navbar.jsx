030caf71dbf0022fc338b2432d9a59ef60cf5546
abol ghana <aghp81@gmail.com>
2024-05-30 20:24:25 +0330

    استایل دادن به لینک ها در کامپوننت Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Navbar(){
     return (
-        <nav>
-            <Link href={'/'}>Abol Ghana</Link>
-            <Link href={'/addTopic'}>افزودن تاپیک</Link>
+        <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
+            <Link className="text-white font-bold" href={'/'}>Abol Ghana</Link>
+            <Link className="bg-white p-2" href={'/addTopic'}>افزودن تاپیک</Link>
         </nav>
     )
 }
