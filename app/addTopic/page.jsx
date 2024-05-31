f4d377589a368f92b3a7845c57409b16ef6d1e70
abol ghana <aghp81@gmail.com>
2024-05-31 23:52:30 +0330

    ایجاد فرم برای افزودن تاپیک

---
+++ app/addTopic/page.jsx
@@ -0,0 +1,5 @@
+export default function AddTopic() {
+    return <form className="flex flex-col gap-3">
+        <input className="border border-slate-500 px-8 py-2" type="text" placeholder="افزودن تاپیک" />
+    </form>
+}
