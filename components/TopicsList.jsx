e493f984445b90c53de9442b47a83fdb44a197ad
abol ghana <aghp81@gmail.com>
2024-05-31 23:22:22 +0330

    عنوان تاپیک و توضیحات

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -2,7 +2,12 @@ export default function TopicsList()
 {
     return (
         <>
-            
+            <div>
+            <h2>عنوان تاپیک</h2>
+                <div>توضیحات تاپیک</div>
+            </div>
+
+            {/* دکمه حذف */}
         </>
     )
 }
