07022047230b6803ba983be866792ab45cdd995a
abol ghana <aghp81@gmail.com>
2024-06-01 22:52:31 +0330

    قرار دادن فرم ویرایش تاپیک

--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -1,3 +1,11 @@
 export default function EditTopicForm() {
-    return <div>ویرایش فرم تاپیک</div>
+    return (
+        <form className="flex flex-col gap-3">
+        <input className="border border-slate-500 px-8 py-2" type="text" placeholder="ویرایش تاپیک" />
+
+        <input className="border border-slate-500 px-8 py-2" type="text" placeholder="توضیحات تاپیک" />
+
+        <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">ویرایش تاپیک</button>
+    </form>
+    );
 }
