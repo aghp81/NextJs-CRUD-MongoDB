28978b218f76fa937d90888b69e9c073955bc062
abol ghana <aghp81@gmail.com>
2024-06-03 20:27:19 +0330

    فچ کردن دیتا از دیتابیس و گرفتن آی دی برای ویرایش

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -2,25 +2,43 @@ import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 
-export default function TopicsList()
+const getTopics = async() => {
+    try {
+        const res = await fetch('http://localhost:3000/api/topics', {
+            cache: "no-store",
+        });
+
+        if( !res.ok ){
+            throw new Error("فراخوانی تاپیک ها موفقیت امیز نبود")
+        }
+        return res.json();
+    } catch (error) {
+        console.log("خطای لود شدن تاپیک ها", error);
+    }
+}
+
+export default async function TopicsList()
 {
+    const {topics} = await getTopics();
     return (
         <>
+        {topics.map(t => (
             <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
                 <div>
-                    <h2 className="font-bold text-2xl">عنوان تاپیک</h2>
-                    <div>توضیحات تاپیک</div>
+                    <h2 className="font-bold text-2xl">{t.title}</h2>
+                    <div>{t.description}</div>
                 </div>
             
 
                 {/* دکمه حذف */}
                 <div className="flex gap-2">
                     <RemoveBtn />
-                    <Link href={'/editTopic/123'}>
+                    <Link href={`/editTopic/${t._id}`}>
                         <HiPencilAlt size={24} />
                     </Link>
                 </div>
             </div>
+            ))} 
         </>
     )
 }
