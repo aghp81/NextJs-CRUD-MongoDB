c814cabeb89a16af4e6c5c624545f10d262c0e74
abol ghana <aghp81@gmail.com>
2024-06-04 18:48:17 +0330

    گرفتن id, title, description از فایل page.jsx در editTopic

--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -1,4 +1,4 @@
-export default function EditTopicForm() {
+export default function EditTopicForm({ id, title, description }) {
     return (
         <form className="flex flex-col gap-3">
         <input className="border border-slate-500 px-8 py-2" type="text" placeholder="ویرایش تاپیک" />
