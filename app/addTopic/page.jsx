648b6f31e020a6205a47b839b6c9fd4c5ee4e4ca
abol ghana <aghp81@gmail.com>
2024-06-04 10:29:47 +0330

    form onSubmit , button type="submit" ,

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -5,8 +5,12 @@ import { useState } from "react"
 export default function AddTopic() {
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
+
+    const handleSubmit = () =>{
+
+    }
     
-    return <form className="flex flex-col gap-3">
+    return <form onSubmit={handleSubmit} className="flex flex-col gap-3">
         <input 
             onChange={(e) => setTitle(e.target.value)}
             value={title}
@@ -17,6 +21,6 @@ export default function AddTopic() {
             value={description}
             className="border border-slate-500 px-8 py-2" type="text" placeholder="توضیحات تاپیک" />
 
-        <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">افزودن تاپیک</button>
+        <button type="submit" className="bg-green-600 font-bold text-white py-3 px-6 w-fit">افزودن تاپیک</button>
     </form>
 }
