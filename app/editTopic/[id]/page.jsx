ed2555b390e4999e91e133709cafd59ea76168fb
abol ghana <aghp81@gmail.com>
2024-06-04 11:20:54 +0330

    دریافت آی دی مربوطه تاپیک

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,5 +1,10 @@
 import EditTopicForm from "@/components/EditTopicForm";
 
-export default function EditTopic() {
+export default function EditTopic({ params }) {
+
+    // دریافت آی دی مربوطه
+    const { id } = params;
+    console.log("id: ", id)
+
     return <EditTopicForm />    
 }
