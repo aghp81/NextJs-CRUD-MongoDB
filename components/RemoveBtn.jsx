7943fc5fbb619aabc07b5e3a52871a91ceaba965
abol ghana <aghp81@gmail.com>
2024-06-04 11:04:27 +0330

    سوال قبل از حذف و پاس دادن آی دی و متد دیلیت

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,6 +1,16 @@
 import { HiOutlineTrash } from "react-icons/hi"
 
 export default function RemoveBtn( {id} ) {
+
+    const removeTopic = async () => {
+        const confirmed = confirm('آیا از حذف مطمئنی؟');
+        if (confirmed) {
+            await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+                method: "DELETE",
+            });
+        }
+    };
+
     return (
         <button className="text-red-400">
             <HiOutlineTrash size={24} />
