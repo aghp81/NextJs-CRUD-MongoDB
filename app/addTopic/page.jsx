ba67943a3b52cfbbd820f021a37635dc0650569f
abol ghana <aghp81@gmail.com>
2024-06-04 10:22:43 +0330

    useState برای title , description

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,6 +1,10 @@
 "use client"
 
+import { useState } from "react"
+
 export default function AddTopic() {
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
     
     return <form className="flex flex-col gap-3">
         <input className="border border-slate-500 px-8 py-2" type="text" placeholder="افزودن تاپیک" />
