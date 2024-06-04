b4c70629b82581c654c3f8807695ab9284df5729
abol ghana <aghp81@gmail.com>
2024-06-04 18:54:16 +0330

    setNewTitle, setNewDescription for input form

--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -9,9 +9,15 @@ export default function EditTopicForm({ id, title, description }) {
     
     return (
         <form className="flex flex-col gap-3">
-        <input className="border border-slate-500 px-8 py-2" type="text" placeholder="ویرایش تاپیک" />
+        <input 
+            onChange={(e) => setNewTitle(e.target.value)}
+            value={newTitle}
+            className="border border-slate-500 px-8 py-2" type="text" placeholder="ویرایش تاپیک" />
 
-        <input className="border border-slate-500 px-8 py-2" type="text" placeholder="توضیحات تاپیک" />
+        <input 
+            onChange={(e) => setNewDescription(e.target.value)}
+            value={newDescription}
+            className="border border-slate-500 px-8 py-2" type="text" placeholder="توضیحات تاپیک" />
 
         <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">ویرایش تاپیک</button>
     </form>
