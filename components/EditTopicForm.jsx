1a56102298e9e33498c6e25ea4acefab407a3771
abol ghana <aghp81@gmail.com>
2024-06-04 18:51:21 +0330

    useState for newTitle, newDescription

--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -1,6 +1,12 @@
 "use client"
 
+import { useState } from "react";
+
 export default function EditTopicForm({ id, title, description }) {
+
+    const [newTitle, setNewTitle] = useState(title)
+    const [newDescription, setNewDescription] = useState(description)
+    
     return (
         <form className="flex flex-col gap-3">
         <input className="border border-slate-500 px-8 py-2" type="text" placeholder="ویرایش تاپیک" />
