597f6f165f406e93c01432d52fac25250c793c6e
abol ghana <aghp81@gmail.com>
2024-06-04 19:03:00 +0330

    method: "PUT", const handleSubmit,

--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -6,9 +6,29 @@ export default function EditTopicForm({ id, title, description }) {
 
     const [newTitle, setNewTitle] = useState(title)
     const [newDescription, setNewDescription] = useState(description)
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+
+        try {
+            const res = await fetch(`http://localhost:3000/api/topics/${id}`,{
+                method: "PUT",
+                headers: {
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify({ newTitle, newDescription }),
+            });
+
+            if (!res.ok) {
+                throw new Error("خطا در بروزرسانی تاپیک")
+            }
+        } catch (error) {
+            
+        }
+    }
     
     return (
-        <form className="flex flex-col gap-3">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
         <input 
             onChange={(e) => setNewTitle(e.target.value)}
             value={newTitle}
