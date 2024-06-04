1ce028d8201b2cea7c0894dbce59ad24cc27dcba
abol ghana <aghp81@gmail.com>
2024-06-04 10:26:32 +0330

    onChange handler for input title and description

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -7,9 +7,15 @@ export default function AddTopic() {
     const [description, setDescription] = useState("")
     
     return <form className="flex flex-col gap-3">
-        <input className="border border-slate-500 px-8 py-2" type="text" placeholder="افزودن تاپیک" />
+        <input 
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            className="border border-slate-500 px-8 py-2" type="text" placeholder="افزودن تاپیک" />
 
-        <input className="border border-slate-500 px-8 py-2" type="text" placeholder="توضیحات تاپیک" />
+        <input 
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            className="border border-slate-500 px-8 py-2" type="text" placeholder="توضیحات تاپیک" />
 
         <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">افزودن تاپیک</button>
     </form>
