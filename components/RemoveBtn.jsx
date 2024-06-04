7d3c395d4c60c32a83a83f954a8e532bab61f046
abol ghana <aghp81@gmail.com>
2024-06-04 11:13:13 +0330

    اگر حذف انجام شد صفحه رفرش شود

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -4,13 +4,18 @@ import { HiOutlineTrash } from "react-icons/hi"
 import { useRouter } from "next/navigation"; // برای ریدایرکت کردن
 
 export default function RemoveBtn( {id} ) {
-
+    const router = useRouter();
     const removeTopic = async () => {
         const confirmed = confirm('آیا از حذف مطمئنی؟');
         if (confirmed) {
-            await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+            const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
                 method: "DELETE",
             });
+            // اگر حذف انجام شد صفحه رفرش شود
+            if (res.ok) {
+                router.refresh()
+            }
+            
         }
     };
 
